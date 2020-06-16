59ccae00b34f9e2fcdebb2432415506c064955cc
CarmineV <carmine.vitale90@gmail.com>
2020-06-16 16:43:40 +0200

    Richiesta esercizio completata

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Student;
+
+class StudentController extends Controller
+{
+    public function index() {
+
+        $students = Student::where('id', '<>', 2)->orderBy('id', 'desc')->limit(2)->get();
+
+        //$students = Student::all()->orderBy('matricola', 'desc')->get();
+
+
+        return view('welcome', compact('students'));
+    }
+}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,5 +8,17 @@
 </head>
 <body>
     <h1>Migrations</h1>
+
+    <main>
+        <h2>I nostri studenti</h2>
+        @foreach($students as $student)
+            <div class="students">
+                <h2>Studente {{ $student->id }}</h2>
+                <h4>Nome e cognome: </h4> {{ $student->name }} {{ $student->surname }}
+                <h4>Presentazione:</h4> {{ $student->description }}
+                <h4>Numero di matricola:</h4> {{ $student->matricola }}
+            </div>
+        @endforeach
+    </main>
 </body>
 </html>
